7f96f2d4bfb7bb8953caa00d795e881330b1fa17
Erdwin Nandayapa <nandayapaerdwin956@gmail.com>
2024-04-16 11:18:31 -0600

    Add enums for HTTP status and response messages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { PORT = 3000 } = process.env;
+const {HttpStatus,HttpResponseMessage} = require("./enums/http")
 
 const usersRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
@@ -10,7 +11,7 @@ app.use("/cards", cardRouter);
 
 
 app.use((req, res) => {
-  res.status(404).send({ message: "Recurso no encontrado" });
+  return res.status(HttpStatus).send({message: HttpResponseMessage.NOT_FOUND});
 });
 
 app.listen(PORT, () => {
